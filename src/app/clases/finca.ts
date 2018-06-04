9ace0acf1b7e35fc04a98322feec2df451f47913
JhonMunarGomez <munar117@hotmail.com>
2018-06-04 14:30:51 -0500

    Prueba

--- src/app/clases/finca.ts
+++ src/app/clases/finca.ts
@@ -1,7 +1,7 @@
 import{HttpClient,HttpHeaders} from '@angular/common/http'
 import { Injectable } from '@angular/core';
 import {Config} from './config'
-
+@Injectable()
 export class Finca{
     codigo_finca?:string;
     nombre_finca:string;
